9c01e4a20e6671bbfdb31e9854888039f94f685a
Anastasia <tildatopdev@gmail.com>
2024-10-10 18:06:02 +0300

    add fix localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import ContactList from "./components/ContactList/ContactList";
 import ContactForm from "./components/ContactForm/ContactForm";
@@ -12,11 +12,22 @@ function App() {
     { id: "id-3", name: "Eden Clements", number: "645-17-79" },
     { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
   ]);
-  const [users, setUsers] = useState(contacts);
+
+  const [users, setUsers] = useState(() => {
+    const stringifiedContact = localStorage.getItem("users");
+    const parsedStringifiedContact = JSON.parse(stringifiedContact) ?? contacts;
+    return parsedStringifiedContact;
+  });
   const [filter, setFilter] = useState("");
+  useEffect(() => {
+    const stringifiedUsers = JSON.stringify(users);
+    localStorage.setItem("users", stringifiedUsers);
+  }, [users]);
   const onAddProfile = (formData) => {
     const finalUsers = { ...formData, id: nanoid() };
     setUsers((prevState) => [...prevState, finalUsers]);
+    localStorage.setItem("users", JSON.stringify(updatedUsers)); // Оновлюємо локальне сховище
+    return updatedUsers;
   };
   const onDeleteProfile = (profileId) => {
     const updatedUsers = users.filter((user) => user.id !== profileId);
@@ -26,6 +37,7 @@ function App() {
   const filteredUsers = users.filter((user) =>
     user.name.toLowerCase().includes(filter.toLowerCase().trim())
   );
+
   return (
     <>
       <div className="wrapper">
