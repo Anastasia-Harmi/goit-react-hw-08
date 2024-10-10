949a716309913ec413fd5c14a03c255899b54e48
Anastasia <tildatopdev@gmail.com>
2024-10-10 20:02:12 +0300

    add fix

--- src/App.jsx
+++ src/App.jsx
@@ -6,35 +6,32 @@ import SearchBox from "./components/SearchBox/SearchBox";
 import Contact from "./components/Contact/Contact";
 import { nanoid } from "nanoid";
 function App() {
-  const [contacts] = useState([
-    { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-    { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-    { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-    { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-  ]);
+  const [contacts, setContacts] = useState(
+    () =>
+      JSON.parse(localStorage.getItem("contacts")) ?? [
+        { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+        { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+        { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+        { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+      ]
+  );
 
-  const [users, setUsers] = useState(() => {
-    const stringifiedContact = localStorage.getItem("users");
-    const parsedStringifiedContact = JSON.parse(stringifiedContact) ?? contacts;
-    return parsedStringifiedContact;
-  });
   const [filter, setFilter] = useState("");
+
   useEffect(() => {
-    const stringifiedUsers = JSON.stringify(users);
-    localStorage.setItem("users", stringifiedUsers);
-  }, [users]);
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  }, [contacts]);
+
   const onAddProfile = (formData) => {
     const finalUsers = { ...formData, id: nanoid() };
-    setUsers((prevState) => [...prevState, finalUsers]);
-    localStorage.setItem("users", JSON.stringify(updatedUsers)); // Оновлюємо локальне сховище
-    return updatedUsers;
+    setContacts((prevState) => [...prevState, finalUsers]);
   };
   const onDeleteProfile = (profileId) => {
-    const updatedUsers = users.filter((user) => user.id !== profileId);
+    const updatedUsers = contacts.filter((user) => user.id !== profileId);
 
-    setUsers(updatedUsers);
+    setContacts(updatedUsers);
   };
-  const filteredUsers = users.filter((user) =>
+  const filteredUsers = contacts.filter((user) =>
     user.name.toLowerCase().includes(filter.toLowerCase().trim())
   );
 
