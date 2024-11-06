f261e5958e73d3d5642e4be2c948f0a6076fa20f
Anastasia <tildatopdev@gmail.com>
2024-11-07 00:23:39 +0200

    Add filterSlice

---
+++ src/redux/filter/filtersSlice.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+const INITIAL_STATE = {
+  name: "",
+};
+
+const filtersSlice = createSlice({
+  name: "filter",
+  initialState: INITIAL_STATE,
+  reducers: {
+    setFilter: (state, action) => {
+      state.name = action.payload;
+    },
+  },
+});
+export const filterReducer = filtersSlice.reducer;
+export const { setFilter } = filtersSlice.actions;
