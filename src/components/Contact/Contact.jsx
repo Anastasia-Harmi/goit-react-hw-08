fdc6c3fadb3ba35da291ccfaa1f76b3e3137a1a0
Anastasia <tildatopdev@gmail.com>
2024-11-07 19:46:16 +0200

    fix

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,21 +2,19 @@ import React from "react";
 import css from "./Contact.module.css";
 const Contact = ({ id, name, number, onDeleteProfile }) => {
   return (
-    <div>
-      <li className={css.li}>
-        <div className={css.divname}>
-          <h3 className={css.titlename}>{name}</h3>
-          <p className={css.number}>{number}</p>
-        </div>
-        <button
-          className={css.btn}
-          type="submit"
-          onClick={() => onDeleteProfile(id)}
-        >
-          Delete
-        </button>
-      </li>
-    </div>
+    <li className={css.li}>
+      <div className={css.divname}>
+        <h3 className={css.titlename}>{name}</h3>
+        <p className={css.number}>{number}</p>
+      </div>
+      <button
+        className={css.btn}
+        type="submit"
+        onClick={() => onDeleteProfile(id)}
+      >
+        Delete
+      </button>
+    </li>
   );
 };
 
