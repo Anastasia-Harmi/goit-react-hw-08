a16fdae5ecbfc0ea84c5dd92beae10d33bec5819
Anastasia <tildatopdev@gmail.com>
2024-11-07 19:30:41 +0200

    fix contactList

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,10 +1,32 @@
 import React from "react";
 import Contact from "../Contact/Contact";
+import { useSelector } from "react-redux";
 import css from "./ContactList.module.css";
-const ContactList = ({ contacts, onDeleteProfile }) => {
+
+const ContactList = ({ onDeleteProfile }) => {
+  const contacts = useSelector((state) => state.contacts.items);
+
+  const filter = useSelector((state) => state.filter.name);
+  const filteredContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
   return (
     <div>
       <ul className={css.ul}>
+        {filteredContacts.length > 0 &&
+          filteredContacts.map((contact) => (
+            <Contact
+              key={contact.id}
+              id={contact.id}
+              userName={contact.name}
+              userPhone={contact.number}
+              userId={contact.id}
+              onDeleteProfile={onDeleteProfile}
+            />
+          ))}
+      </ul>
+      {/* <ul className={css.ul}>
         {contacts.map((contact) => {
           return (
             <Contact
@@ -16,7 +38,7 @@ const ContactList = ({ contacts, onDeleteProfile }) => {
             />
           );
         })}
-      </ul>
+      </ul> */}
     </div>
   );
 };
