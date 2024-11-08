c12f2388c6c9ada3c6bcd3321a3c965803c3f23a
Anastasia <tildatopdev@gmail.com>
2024-11-08 11:41:51 +0200

    add persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,37 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { contactsReducer } from "./contacts/contactsSlice";
 import { filterReducer } from "./filter/filtersSlice";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const contactsConfig = {
+  key: "contactsKey",
+  storage,
+  whitelist: ["contacts"],
+  // blacklist: ["showProfilesList"]
+};
 
 export const store = configureStore({
   reducer: {
-    contacts: contactsReducer,
+    contacts: persistReducer(contactsConfig, contactsReducer),
     filter: filterReducer,
   },
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
