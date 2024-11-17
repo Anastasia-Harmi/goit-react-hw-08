45fd34063e5d6abe511dbb654d19c78f8aad12a0
Anastasia <tildatopdev@gmail.com>
2024-11-17 14:34:52 +0200

    add register form

--- src/pages/RegistrationPage/RegistrationPage.jsx
+++ src/pages/RegistrationPage/RegistrationPage.jsx
@@ -1,7 +1,79 @@
+import { ErrorMessage, Field, Form, Formik } from "formik";
 import React from "react";
+import { RegisterUserSchema } from "../../utils/schemas";
+import { register } from "../../redux/auth/operations";
+import css from "./RegistrationPage.module.css";
+import { useDispatch } from "react-redux";
+const INITIAL_VALUES = {
+  name: "",
+  email: "",
+  password: "",
+};
 
 const RegistrationPage = () => {
-  return <div>RegistrationPage</div>;
+  const dispatch = useDispatch();
+
+  const handleSubmit = (values, actions) => {
+    dispatch(register(values));
+    actions.resetForm();
+  };
+
+  return (
+    <div>
+      <Formik
+        initialValues={INITIAL_VALUES}
+        validationSchema={RegisterUserSchema}
+        onSubmit={handleSubmit}
+      >
+        <Form className={css.form}>
+          <label className={css.label}>
+            <span>Name:</span>
+            <Field
+              type="text"
+              name="name"
+              className={css.input}
+              placeholder="Ivan Shevchenko"
+            />
+            <ErrorMessage
+              className={css.errorMessage}
+              name="name" //вказуємо той самий name до якого прив'язуєм
+              component="span"
+            />
+          </label>
+          <label className={css.label}>
+            <span>Email:</span>
+            <Field
+              type="text"
+              name="email"
+              className={css.input}
+              placeholder="example.email@example.com"
+            />
+            <ErrorMessage
+              className={css.errorMessage}
+              name="email"
+              component="span"
+            />
+          </label>
+          <label className={css.label}>
+            <span>Password:</span>
+            <Field
+              type="password"
+              name="password"
+              className={css.input}
+              placeholder="Enter your password"
+            />
+            <ErrorMessage
+              className={css.errorMessage}
+              name="password"
+              component="span"
+            />
+          </label>
+
+          <button type="submit"> Register</button>
+        </Form>
+      </Formik>
+    </div>
+  );
 };
 
 export default RegistrationPage;
